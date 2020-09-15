1afa9876f385830b671c1bfe92f859795b6559c2
leo <leo09613257@gmail.com>
2020-09-15 09:19:00 +0800

    modify error

--- chat.py
+++ chat.py
@@ -11,7 +11,7 @@ def read_file (filename) :
 
 def convert (lines) :
     
-    new = []
+    new= []
     person = None
     for line in lines :
         if line == 'Miko' :  
@@ -20,20 +20,21 @@ def convert (lines) :
         elif line == 'Leo' : #到leo 會取代
             person = 'Leo'
             continue
-    if person :
-        new.append(person + ': ' + line)   #要在loop裡面 不然會迭代  
+        
+        if person :
+            new.append(person + ': ' + line)   #1.要在loop裡面 不然會迭代前面 2.要存成new
     return new
     
 
 def write_file(filename , lines) :
-    with open (filename , 'w' ,encoding='utf-8') as f :
+    with open (filename , 'w' ,encoding='utf-8-sig') as f :
         for line in lines :
             f.write(line + '\n')
 
 
 def main() :
     lines = read_file('input.txt')
-    convert(lines)
+    lines = convert(lines)
     write_file('output.txt' ,lines)
 
 main()
