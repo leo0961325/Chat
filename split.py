88a2727c7d0503ffce92475a46b925f26f4a2a6a
leo0961325 <47008876+leo0961325@users.noreply.github.com>
2020-09-24 22:16:43 +0800

    Add files via upload

---
+++ split.py
@@ -0,0 +1,11 @@
+lines = []
+with open ('split.txt' , 'r' , encoding='utf-8-sig') as f :
+    for line in f :
+        lines.append(line.strip())
+        
+        
+for line in lines :
+    s = line.split(' ')
+    time = s[0][:5]
+    name = s[0][5:]
+    print(name)    
